1ddae347903718dbe158c2c7d93be7a84f6f1d0f
DNA.LS <NizakiFX@gmail.com>
2021-04-14 23:19:10 +0700

    add sound tweak AI

--- Assets/Scripts/InterObject.cs
+++ Assets/Scripts/InterObject.cs
@@ -1,59 +1,195 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using MyBox;
+[RequireComponent(typeof(AudioSource))]
 public class InterObject : MonoBehaviour, IDamagable
 {
 
+  bool alive = true;
+  public float Hp = 10;
+  public float MaxHP = 10;
+  public EquipType EffectiveType = EquipType.None;
+  public bool hasAI = true;
 
-    public float Hp = 10;
-    public float MaxHP = 10;
-    public EquipType EffectiveType = EquipType.None;
-    [SerializeField]
-    public List<DropItem> DropList;
-    private void Start()
+  [ConditionalField("hasAI")]
+  public AI ai = AI.Idle;
+  [ConditionalField("hasAI")]
+  public float FollowDistance = 5f;
+  [ConditionalField("hasAI")]
+  public float attackRange = 5f;
+  [SerializeField]
+  public List<DropItem> DropList;
+  GameObject player;
+  Vector2 targetPos;
+  bool moving = false;
+  [ConditionalField("hasAI")]
+  public float attackDelay = 5f;
+  float currectAttackDelay = 0f;
+  public AudioClip hit, die;
+  AudioSource source;
+  public virtual void Start()
+  {
+    player = GameObject.FindGameObjectWithTag("Player");
+    Hp = MaxHP;
+    if (attackRange >= FollowDistance)
+      attackRange = FollowDistance / 2f;
+    StartCoroutine(nameof(AiSquence));
+    source = GetComponent<AudioSource>();
+    if (!hasAI)
     {
-        Hp = MaxHP;
+      FollowDistance = 0;
+      attackRange = 0;
     }
-    public void TakeDamage(int damage, EquipType sourceEquip = EquipType.None)
+  }
+
+  public void TakeDamage(int damage, EquipType sourceEquip = EquipType.None)
+  {
+    source.PlayOneShot(hit);
+    if (sourceEquip == EffectiveType)
     {
-        if(sourceEquip == EffectiveType)
-        {
-            Hp -= 5;
-        }
-        else
-        {
-            Hp -= 2;
-        }
-        Debug.Log($"Take hit HP ramin {Hp}/{MaxHP}");
-        if (Hp <= 0)
-        {
-            Die();
-        }
+      Hp -= 5;
     }
-
-    void Die()
+    else
     {
-        Debug.Log("Die");
+      Hp -= 2;
+    }
+    Debug.Log($"{this.name} ({Hp}/{MaxHP})");
+    if (Hp <= 0)
+    {
+      StartCoroutine(nameof(Die));
+    }
+  }
 
-        foreach (DropItem item in DropList)
-        {
-            if(item.chance > Random.Range(0, 100))
+  private void LateUpdate()
+  {
+  }
+
+  private void FixedUpdate()
+  {
+    currectAttackDelay -= 1 * Time.fixedDeltaTime;
+    if (currectAttackDelay < 0)
+    {
+      currectAttackDelay = 0;
+    }
+    if (moving)
+    {
+      transform.position = Vector2.MoveTowards(transform.position, targetPos, 5 * Time.fixedDeltaTime);
+    }
+  }
+
+  IEnumerator AiSquence()
+  {
+    while (alive && hasAI)
+    {
+      float distance = Vector2.Distance(transform.position, player.transform.position);
+      switch (ai)
+      {
+        case AI.Idle:
+          if (distance < FollowDistance)
+          {
+            ai = AI.Follow;
+            targetPos = player.transform.position;
+          }
+          else
+          {
+            yield return new WaitForSeconds(Random.Range(1f, 3f));
+            ai = AI.Wander;
+            //random pick location
+            targetPos = new Vector2(transform.position.x + Random.Range(-5f, 5f), transform.position.y + Random.Range(-5f, 5f));
+            moving = true;
+          }
+          break;
+        case AI.Wander:
+          float distance2 = Vector2.Distance(transform.position, targetPos);
+          while (moving)
+          {
+            Debug.Log("Move to random path");
+            distance2 = Vector2.Distance(transform.position, targetPos);
+            if (distance2 < 2)
             {
-                //TODO : spawn item that player can pickup;
-                Debug.Log($"Drop {item.item.name}");
+              moving = false;
+              ai = AI.Idle;
             }
-        }
 
-        Destroy(this.gameObject);
+            yield return new WaitForEndOfFrame();
+          }
+          break;
+        case AI.Follow:
+          if (distance > FollowDistance)
+          {
+            moving = false;
+            ai = AI.Idle;
+          }
+          else if (distance < attackRange)
+          {
+            moving = false;
+            ai = AI.Attack;
+          }
+          else
+          {
+            targetPos = player.transform.position;
+            Debug.Log($"follow player to {targetPos}");
+            moving = true;
+          }
+          break;
+        case AI.Attack:
+          if (currectAttackDelay <= 0)
+          {
+            player.GetComponent<Player>().TakeDamage(5);
+            currectAttackDelay = attackDelay;
+          }
+          yield return new WaitForSeconds(0.5f);
+          ai = AI.Follow;
+          break;
+        default:
+          break;
+      }
+      yield return new WaitForEndOfFrame();
     }
+  }
+
+  IEnumerator Die()
+  {
+    Debug.Log("Die");
+    alive = false;
+    foreach (DropItem item in DropList)
+    {
+      if (item.chance > Random.Range(0, 100))
+      {
+        //TODO : spawn item that player can pickup;
+        Debug.Log($"Drop {item.item.name}");
+      }
+    }
+    yield return new WaitForSeconds(1f);
+    Destroy(this.gameObject);
+  }
+  private void OnDrawGizmos()
+  {
+    if (hasAI)
+    {
+      Gizmos.color = Color.black;
+      Gizmos.DrawWireSphere(transform.position, FollowDistance);
+      Gizmos.color = Color.red;
+      Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+  }
+}
+
+
+public enum AI
+{
+  Idle,   // just stand still
+  Wander, // after wander back to idle
+  Follow, // when found player switch to follow imediatly depen on ai
+  Attack, // when in range try to attck after attack check if can attack another time if not switch to follow
 }
 
 [System.Serializable]
 public class DropItem
 {
-    [SerializeField,Range(0,100)]
-    public int chance;
-    [SerializeField]
-    public ItemStack item;
+  [SerializeField, Range(0, 100)]
+  public int chance;
+  [SerializeField]
+  public ItemStack item;
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,180 +5,182 @@ using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    public bool alive = true;
-    public int Hp = 100;
-    public int MaxHP = 100;
-    public float Hunger = 100f;
-    public float MaxHunger = 100f;
-    public float HungerDrainRate = 0.1f;
-    public float Water = 100f;
-    public float MaxWater = 100f;
-    public float WaterDrainRate = 0.1f;
-    public float maxDarkDelay = 3;
-    [HideInInspector]
-    public UnityEvent onDeath;
-    [HideInInspector]
-    public UnityEvent<int> onTakeDamage;
-    public float darkDelay = 5;
-    [HideInInspector]
-    public List<GameObject> Interactable = new List<GameObject>();
-    [HideInInspector]
-    public bool UnderLight;
-    float timer;
-    [SerializeField]
-    GameObject Holder;
-    [SerializeField]
-    Transform attackPos;
-    [SerializeField]
-    float attackRange;
-    public LayerMask enemy;
-    // Start is called before the first frame update
-    void Start()
+  public bool alive = true;
+  public int Hp = 100;
+  public int MaxHP = 100;
+  public float Hunger = 100f;
+  public float MaxHunger = 100f;
+  public float HungerDrainRate = 0.1f;
+  public float Water = 100f;
+  public float MaxWater = 100f;
+  public float WaterDrainRate = 0.1f;
+  public float maxDarkDelay = 3;
+  [HideInInspector]
+  public UnityEvent onDeath;
+  [HideInInspector]
+  public UnityEvent<int> onTakeDamage;
+  public float darkDelay = 5;
+  [HideInInspector]
+  public List<GameObject> Interactable = new List<GameObject>();
+  [HideInInspector]
+  public bool UnderLight;
+  float timer;
+  [SerializeField]
+  GameObject Holder;
+  [SerializeField]
+  Transform attackPos;
+  [SerializeField]
+  float attackRange;
+  public Animator punchAnim;
+  public LayerMask enemy;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Hp = MaxHP;
+    Hunger = MaxHunger;
+    Water = MaxWater;
+    if (onDeath == null)
+      onDeath = new UnityEvent();
+    if (onTakeDamage == null)
+      onTakeDamage = new UnityEvent<int>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (!alive)
+      return;
+
+
+    //if (Hp <= 0)
+    //{
+    //    //TODO: แยกฟังค์ชั้น ตายแล้ว ต้องควบคุมไม่ได้
+    //    Debug.Log("Death");
+    //    alive = false;
+    //    onDeath.Invoke();
+    //}
+    UpdateLight();
+    MoveTarget();
+  }
+
+  void UpdateLight()
+  {
+    if (UnderLight || GameTime.state == TimeState.DAY)
     {
-        Hp = MaxHP;
-        Hunger = MaxHunger;
-        Water = MaxWater;
-        if (onDeath == null)
-            onDeath = new UnityEvent();
-        if (onTakeDamage == null)
-            onTakeDamage = new UnityEvent<int>();
+      darkDelay += 0.25f * Time.deltaTime;
+      if (darkDelay > maxDarkDelay)
+        darkDelay = maxDarkDelay;
+      return;
     }
 
-    // Update is called once per frame
-    void Update()
+    if (GameTime.state == TimeState.NiGHT && !UnderLight)
     {
-        if (!alive)
-            return;
-
-
-        //if (Hp <= 0)
-        //{
-        //    //TODO: แยกฟังค์ชั้น ตายแล้ว ต้องควบคุมไม่ได้
-        //    Debug.Log("Death");
-        //    alive = false;
-        //    onDeath.Invoke();
-        //}
-        UpdateLight();
-        MoveTarget();
-    }
-
-    void UpdateLight()
-    {
-        if (UnderLight || GameTime.state == TimeState.DAY)
+      darkDelay -= Time.deltaTime;
+      if (darkDelay <= 0)
+      {
+        timer += 1 * Time.deltaTime;
+        if (timer > 3f)
         {
-            darkDelay += 0.25f * Time.deltaTime;
-            if (darkDelay > maxDarkDelay)
-                darkDelay = maxDarkDelay;
-            return;
+          TakeDamage(25);
+          timer = 0;
         }
+      }
+      if (darkDelay < 0)
+      {
+        darkDelay = 0;
+      }
 
-        if (GameTime.state == TimeState.NiGHT && !UnderLight)
-        {
-            darkDelay -= Time.deltaTime;
-            if (darkDelay <= 0)
-            {
-                timer += 1 * Time.deltaTime;
-                if (timer > 3f)
-                {
-                    TakeDamage(25);
-                    timer = 0;
-                }
-            }
-            if(darkDelay < 0)
-            {
-                darkDelay = 0;
-            }
-
-        }
     }
+  }
 
-    void MoveTarget()
+  void MoveTarget()
+  {
+    Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    Vector3 dir = pos - Holder.transform.position;
+    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+    Holder.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+  }
+
+  public void Attack()
+  {
+    punchAnim.SetTrigger("punch");
+    Collider2D[] toDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange);
+    Debug.Log($"Attack found {toDamage.Length}");
+    foreach (Collider2D item in toDamage)
     {
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 dir = pos - Holder.transform.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        Holder.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+      IDamagable damagable = item.gameObject.GetComponent<InterObject>();
+      if (damagable != null && !item.isTrigger)
+      {
+        damagable.TakeDamage(5);
+      }
     }
+  }
 
-    public void Attack()
-    {
-        Collider2D[] toDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange);
-        Debug.Log($"Attack found {toDamage.Length}");
-        foreach (Collider2D item in toDamage)
-        {
-            IDamagable damagable = item.gameObject.GetComponent<InterObject>();
-            if (damagable != null && !item.isTrigger)
-            {
-                damagable.TakeDamage(5);
-            }
-        }
-    }
+  public void TakeDamage(int value)
+  {
+    Hp -= value;
+    if (Hp <= 0)
+      Debug.Log("ตาย");
 
-    void TakeDamage(int value)
-    {
-        Hp -= value;
-        if (Hp <= 0)
-            Debug.Log("ตาย");
+    onTakeDamage.Invoke(value);
+  }
 
-        onTakeDamage.Invoke(value);
-    }
+  private void FixedUpdate()
+  {
+    Hunger -= HungerDrainRate * Time.fixedDeltaTime;
+    Water -= WaterDrainRate * Time.fixedDeltaTime;
+  }
+  private void OnDrawGizmos()
+  {
+    Gizmos.color = Color.red;
+    Gizmos.DrawWireSphere(attackPos.position, attackRange);
+  }
 
-    private void FixedUpdate()
+  GameObject GetClosetInteractable()
+  {
+    GameObject result = null;
+    float minDist = Mathf.Infinity;
+    Vector2 currentPos = transform.position;
+    foreach (var go in Interactable)
     {
-        Hunger -= HungerDrainRate * Time.fixedDeltaTime;
-        Water -= WaterDrainRate * Time.fixedDeltaTime;
+      float dist = Vector2.Distance(go.transform.position, currentPos);
+      if (dist < minDist)
+      {
+        result = go;
+        minDist = dist;
+      }
     }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position,attackRange);
-    }
+    return result;
+  }
 
-    GameObject GetClosetInteractable()
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.CompareTag("Interactive"))
     {
-        GameObject result = null;
-        float minDist = Mathf.Infinity;
-        Vector2 currentPos = transform.position;
-        foreach (var go in Interactable)
-        {
-            float dist = Vector2.Distance(go.transform.position, currentPos);
-            if (dist < minDist)
-            {
-                result = go;
-                minDist = dist;
-            }
-        }
-        return result;
+      Interactable.Add(collision.gameObject);
     }
+  }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    if (collision.CompareTag("Interactive"))
     {
-        if (collision.CompareTag("Interactive"))
-        {
-            Interactable.Add(collision.gameObject);
-        }
+      Interactable.Remove(collision.gameObject);
     }
+  }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Interactive"))
-        {
-            Interactable.Remove(collision.gameObject);
-        }
-    }
+  public void Heal(int value)
+  {
+    Hp += value;
+  }
 
-    public void Heal(int value)
-    {
-        Hp += value;
-    }
+  public void Eat(int value)
+  {
+    Hunger += value;
+  }
 
-    public void Eat(int value)
-    {
-        Hunger += value;
-    }
-
-    public void Drink(int value)
-    {
-        Water += value;
-    }
+  public void Drink(int value)
+  {
+    Water += value;
+  }
 }
